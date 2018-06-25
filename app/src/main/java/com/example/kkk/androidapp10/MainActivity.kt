8fddc7d636c4e2f0e6d3251ebf6c07ce5955ec3c
Karthikeyan <kkarthikeyan1998@gmail.com>
2018-06-25 09:27:26 +0530

    First commit

--- app/src/main/java/com/example/kkk/androidapp10/MainActivity.kt
+++ app/src/main/java/com/example/kkk/androidapp10/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,23 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         message.text = website
-
-       // val intent = Intent(this,SecondActivity::class.java)
         val uri = Uri.parse(website)
-        val intent = Intent(Intent.ACTION_VIEW,uri)
-        /*
-        message.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-           //     val intent = Intent(this,SecondActivity::class.java)
-                v?.setBackgroundColor(Color.YELLOW)
-                startActivity(intentt)
-            }
-        })
-*/
+        val intent1 = Intent(Intent.ACTION_VIEW,uri)
+
         message.setOnClickListener({ v ->  v.setBackgroundColor(Color.YELLOW)
-                                            startActivity(intent)
+                                            startActivity(intent1)
                                     })
 
+        buttonfirst.text = "Click to go"
+        val intent2 = Intent(this,SecondActivity::class.java)
+        buttonfirst.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(intent2)
+            }
+        })
 
 
 
--- app/src/main/java/com/example/kkk/androidapp10/SecondActivity.kt
+++ app/src/main/java/com/example/kkk/androidapp10/SecondActivity.kt
@@ -2,11 +2,20 @@ package com.example.kkk.androidapp10
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        buttonsecond.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                finish()
+            }
+
+        })
     }
 }
